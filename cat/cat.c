776c1947fbbef55dbc475fe71a09d16c1554a0eb
AReesha <a-rifa@ya.ru>
2016-03-15 02:37:30 +0300

    Added files via upload
    Код отформатирован

--- cat/cat.c
+++ cat/cat.c
@@ -2,19 +2,21 @@
 #include <unistd.h>
 int  main()
 {
-  char buff[1024];
+    char buff[1024];
 	int i = 1;
 	while(i > 0){
-	i = read(0, buff, 1024);
-	if(i>0){
-	int j = write(1, buff, i);
-	if(j<0){
-	write(2, "Error in write\n", 16);
-}
-}
-}
+        i = read(0, buff, 1024);
+        int k = 0;
+        while (i>k){
+            int j = write(1, buff+k, i-k);
+            if(j<0){
+                write(2, "Error in write\n", 16);
+            }
+            k+=j;
+        }
+    }
 	if(i<0){
 	write(2, "Error in read\n", 15);
-}
+    }
     return 0;
 }
